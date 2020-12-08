52efa838ffdf96828f7567e0e58e223862fbad08
Eric Greene <eric@t4d.io>
2020-12-08 15:26:18 -0500

    Add code demos

--- ts-demo/src/index.ts
+++ ts-demo/src/index.ts
@@ -3,15 +3,33 @@ const delay = () => Math.floor(Math.random() * 1000) + 500;
 
 type MyFetch = <T>(url: string) => Promise<T>;
 
+type AjaxError = {
+  status: number;
+  message: string;
+}
+
 const myFetch: MyFetch = <S>(url: string) => {
 
-  return new Promise<S>(resolve => {
+  return new Promise<S>((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange', () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        resolve(JSON.parse(xhr.responseText));
-      }
+      try {
+        if (xhr.readyState === 4 && (xhr.status === 0 || xhr.status >= 400)) {
+          reject({
+            status: xhr.status,
+            message: xhr.statusText,
+          });
+        }
 
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        }
+      } catch (err) {
+        reject({
+          status: 0,
+          message: err,
+        });
+      }
     });
     xhr.open('GET', url);
     xhr.send();
@@ -25,7 +43,12 @@ type Color = {
   hexcode: string;
 }
 
-myFetch<Color[]>('http://localhost:3000/api/colors')
+myFetch<Color[]>('http://localhost:8000/api/colors')
   .then((colors) => {
     console.log(colors);
+  })
+  .catch(err => {
+    const ajaxError = err as AjaxError;
+    console.log('handled error');
+    console.log(ajaxError.status, ajaxError.message);
   });
