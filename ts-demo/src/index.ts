58ba0117563fe04b874fe332908a2a8442faeac9
Eric Greene <eric@t4d.io>
2020-12-08 15:14:45 -0500

    add code demos

--- ts-demo/src/index.ts
+++ ts-demo/src/index.ts
@@ -1,26 +1,31 @@
 
 const delay = () => Math.floor(Math.random() * 1000) + 500;
 
-const xhr = new XMLHttpRequest();
+type MyFetch = <T>(url: string) => Promise<T>;
 
-xhr.addEventListener('readystatechange', () => {
+const myFetch: MyFetch = <S>(url: string) => {
 
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+  return new Promise<S>(resolve => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText));
+      }
 
-});
+    });
+    xhr.open('GET', url);
+    xhr.send();
+  });
 
-xhr.open('GET', 'http://localhost:3000/api/colors');
-xhr.send();
+};
 
-// Write some code such that if the following line of code
-// was pasted into your code file, the line of code would
-// work with no changes.
-// Use the XHR code above to make the AJAX call, do not
-// use the built-in fetch API
+type Color = {
+  id: number;
+  name: string;
+  hexcode: string;
+}
 
-myFetch('http://localhost:3000/api/colors')
-  .then((colors: any[]) => {
+myFetch<Color[]>('http://localhost:3000/api/colors')
+  .then((colors) => {
     console.log(colors);
   });
